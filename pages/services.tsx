0c710bb8acaf37efd4090440fb6ac2e3e35497ef
noumankhandatu <noumankhandatu>
2022-07-13 15:08:58 +0500

    technology cards added

--- pages/services.tsx
+++ pages/services.tsx
@@ -5,10 +5,50 @@ import Form from "../scr/components/organism/form";
 import { AiOutlineRight } from "react-icons/ai";
 import ItemCarousel from "../scr/components/organism/carousel";
 import DeploymentSevicesBanner from "../scr/components/organism/deploymentSevicesBanner";
-import Button from '../scr/components/molecule/button';
+import Button from "../scr/components/molecule/button";
+import AppFont from "../scr/components/atom/AppFont";
+import TechnologyCard from "../scr/components/molecule/TechnologyCard";
 const Services = () => {
   const cssProps =
     "text-[16px] font-medium text-App-orange mr-8  cursor-pointer";
+  const TechnologyCardData = [
+    {
+      title: "Android      ",
+      description:
+        "NinjaTech is not new or naïve in this industry, with the skill-proven Android developers build your application with us.",
+      img: "https://ninjatech.agency/wp-content/uploads/2022/06/android-1.png",
+    },
+    {
+      title: "iOs",
+      description:
+        "We have a great team in iOS development; NinjaTech has adept 130+ projects with the iOS and still counting.",
+      img: "https://ninjatech.agency/wp-content/uploads/2022/06/apple-black-logo.png",
+    },
+    {
+      title: "Cross-platform",
+      description:
+        "It's the bridge between the two platforms to build your app, and making it available on different platforms is now possible with cross-platform technology.",
+      img: "https://ninjatech.agency/wp-content/uploads/2022/06/cross-platform.png",
+    },
+    {
+      title: "Front-end      ",
+      description:
+        "With the expert team of Front-end Developers, now you can make your requirement into reality.",
+      img: "https://ninjatech.agency/wp-content/uploads/2022/06/ux.png",
+    },
+    {
+      title: "Backend",
+      description:
+        "Our developers are proficient in developing the backend profile of your website solid and reliable.",
+      img: "https://ninjatech.agency/wp-content/uploads/2022/06/backend.png",
+    },
+    {
+      title: "Artificial Intelligence",
+      description:
+        "Get the leverage to accelerate data-driven decisions with Artificial Intelligence solutions.",
+      img: "https://ninjatech.agency/wp-content/uploads/2022/06/ai.png",
+    },
+  ];
   return (
     <div>
       <ExpectMoreBanner
@@ -67,11 +107,11 @@ const Services = () => {
                   <p className={`${cssProps}`}>Legacy_App_Modernisation</p>
                   <p className={`${cssProps}`}>Research_and_Development</p>
                 </div>
-                <p className="text-[16px]">
+                <AppFont>
                   The idea begins with imagination, and at NinjaTech, we plan
                   vital moves with substantial business results from inside the
                   association. To deliver progress significant for everyone.
-                </p>
+                </AppFont>
               </div>
               <img
                 className=""
@@ -105,13 +145,13 @@ const Services = () => {
                   <p className={`${cssProps}`}>Computer_Vision</p>
                   <p className={`${cssProps}`}>Voice_Assistants_&_Chatbots</p>
                 </div>
-                <p className="text-[16px]">
+                <AppFont>
                   The innovation can revaluate business processes, make better
                   client encounters, and increase and improve independent human
                   direction. It is seen as perhaps the most significant space of
                   chance for some organizations today – and a central part of
                   most computerized change endeavours.
-                </p>
+                </AppFont>
               </div>
               <img
                 className=""
@@ -160,10 +200,85 @@ const Services = () => {
               Need your idea verified fast?
             </p>
           </div>
-         <div className="flex justify-center pb-20 pt-10"> 
-          <Button variant="contained" className="rounded filled" endIcon={<AiOutlineRight />}>
-            Get a Quote
-          </Button></div>
+          <div className="flex justify-center pb-20 pt-10">
+            <Button
+              variant="contained"
+              className="rounded filled"
+              endIcon={<AiOutlineRight />}
+            >
+              Get a Quote
+            </Button>
+          </div>
+        </div>
+      </Wrapper>
+      <Wrapper full className={"bg-[#F5F5F5]"}>
+        {/* little blue banner */}
+        <Wrapper contained className="p-4">
+          <div>
+            <div className="mt-20 mb-20 ">
+              <div className="grid grid-cols-[1fr,1fr] justify-center  justify-items-center	  items-center">
+                <div>
+                  <p className="text-[36px]">
+                    Our expertise covers many industries
+                  </p>
+                  <div className="flex   mt-5 mb-8 ">
+                    <p className={`${cssProps} bg-white p-2 rounded`}>
+                      Healthcare
+                    </p>
+                    <p className={`${cssProps} p-2`}>E-commerce</p>
+                    <p className={`${cssProps} p-2`}>Entertainment</p>
+                    <p className={`${cssProps} p-2`}>Finetech</p>
+                    <p className={`${cssProps} p-2`}>Others</p>
+                  </div>
+                  <AppFont>
+                    Our plan, programming, content, and advertising group
+                    invests wholeheartedly in its work and has phenomenal
+                    abilities in creating worldwide healthcare & beauty brands
+                    and making advanced encounters for their audience.
+                  </AppFont>
+                </div>
+                <img
+                  className=""
+                  src="https://ninjatech.agency/wp-content/uploads/2022/06/industries-img.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </Wrapper>
+      </Wrapper>
+      {/* Techonology Cards */}
+      <Wrapper contained className="p-32">
+        <div className="grid grid-cols-[1fr,1.4fr] gap-10 items-center">
+          <div>
+            <p className="text-[42px] font-normal">
+              Let's choose the best technology for you
+            </p>
+            <AppFont className="mt-8 mb-8">
+              When it's come to building a website or an application, we have
+              the privilege to choose which technology will help us to develop
+              our digital brand in the way we have been imagined. With a team of
+              experienced and certified developers and designers, we have been
+              leading in the market for the last decade.
+            </AppFont>
+            <AppFont className="mb-5">
+              Make your website look dazzling and smooth by keeping the backend
+              and front-end strong. We have an Excellency in building Android
+              and iOS applications and in cross-platform.
+            </AppFont>
+            <Button
+              variant="contained"
+              endIcon={<AiOutlineRight />}
+              className="filled mt-12 rounded"
+            >
+              Get a Quote
+            </Button>
+          </div>
+          <div className="grid grid-cols-3 gap-5 rounded ">
+            {TechnologyCardData.map((items) => {
+              return <TechnologyCard items={items} />;
+            })}
+          </div>
         </div>
       </Wrapper>
       <Form />
--- scr/components/atom/AppFont.tsx
+++ scr/components/atom/AppFont.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 type AppFontProps = {
   children: string;
-  className: string;
+  className?: string;
   startIcon?: any;
   endIcon?: any;
 };
 const AppFont = ({ children, className, endIcon, startIcon }: AppFontProps) => {
   return (
     <div
-      className={`${className} text-[15px] font-normal text-{#1C1B1B} flex `}
+      className={`${className} text-[16px] font-normal text-{#1C1B1B} flex `}
     >
       {startIcon && <div className={`${className} mt-1`}>{startIcon}</div>}
       {children}
